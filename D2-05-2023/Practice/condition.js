3e1cd51dbb5e918ed8e6bfd8fdc583e9bc3e65dc
benish <benish-ben>
2023-12-01 16:49:45 +0530

    day 4 and 5

---
+++ D2-05-2023/Practice/condition.js
@@ -0,0 +1,50 @@
+// let number=20;
+// console.log(number)
+// if(number==10){
+//     console.log("Numbers are equal")
+// }
+// else{
+//     console.log("Numbers are not equal")
+// }
+
+
+// let a=2;
+// console.log(a)
+// if(a >=5){
+//     console.log("you can buy a choclate")
+// }
+// else{
+//     console.log("you cant buy a choclate")
+//
+// console.log(a)
+// if(a >=5){
+//     console.log("you can buy a choclate")
+// }
+// else{
+//     console.log("you cant buy a choclate")
+// }
+
+
+
+// let son=19
+// let father=45
+// if (son==18 && father==45){
+//     console.log("come inside")
+// }
+// else{
+//     console.log("come outside")
+// }
+
+
+let a="milk"
+if (a=="butter"){
+    console.log(a,"this is butter")
+
+}
+else if(a=="curd"){
+    console.log(a,"this is curd")
+}
+else if(a=="milk"){
+    console.log(a,"this is milk")
+}
+
---
+++ D2-05-2023/Practice/index.js
@@ -0,0 +1,24 @@
+// var number=10
+// console.log(number)
+
+// var a=10
+// console.log(typeof(a))
+
+// var a=10.1
+// console.log(typeof(a))
+
+// var a='B'
+// console.log(a)
+
+// var a="Benish";
+// console.log(a)
+
+// var a= true
+// console.log(a)
+
+// const b=10
+// const c=20
+
+let a=20
+let a=30
+console.log(a)
